fec8c2457f2f81926818e66edef8f9b7be2ca008
Abdur Rahman Shafi <abdur.rahman.shafi@g.bracu.ac.bd>
2025-09-17 14:23:39 +0600

    project initialized

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className=" p-5 text-3xl font-bold underline">Hello, Zeta!</h1>
+    </div>
+  );
+}
